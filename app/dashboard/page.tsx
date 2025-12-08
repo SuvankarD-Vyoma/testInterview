0a28d46ef028a2980f29e66e4a22e13e1bdd3deb
SuvankarD-Vyoma <suvankar.d@vyomainnovusglobal.com>
2025-12-08 17:25:52 +0530

    chenged a bit

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,4 +1,5 @@
-export const dynamic = 'force-dynamic';
+// export const dynamic = 'force-dynamic';
+"use client"
 
 import { Users, ShieldCheck, ClipboardList, Activity, TrendingUp, Clock, CheckCircle2, AlertCircle } from "lucide-react"
 
